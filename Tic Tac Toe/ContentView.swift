7ad1cd70d4c065bf1d0e2b5807e083a33df98ced
Mook <natthaporn.wim@dome.tu.ac.th>
2021-08-27 22:18:21 +0700

    Initial Commit

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Natthaporn Wimonanupong on 27/8/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Natthaporn Wimonanupong on 27/8/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
